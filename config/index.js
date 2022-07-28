089f99e35d0717ddc1395a9758c8fc02d0bbc3f3
fRanaRfs <105783080+fRanaRfs@users.noreply.github.com>
2022-07-28 09:33:04 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,10 +18,7 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   app.use(
-    cors({
-      credentials: true,
-      origin: process.env.ORIGIN || 'http://localhost:3000'
-    })
+    cors()
   );
   
 
